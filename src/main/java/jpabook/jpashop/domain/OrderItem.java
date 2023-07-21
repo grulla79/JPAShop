6f43d9d1fb53c7851d487d3fc905b44679f74cc6
timetable5678 <coolcats6762@gmail.com>
2023-07-21 22:39:48 +0900

    주문 도메인 개발 마무리

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -24,4 +24,23 @@ public class OrderItem {
     private int orderPrice;
     private int count;
 
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //비지니스 로직 - 재고 수량 증가
+    public void cancel(){
+        getItem().addStock(count);
+    }
+
+    public int getTotalPrice(){
+        return getOrderPrice() * getCount();
+    }
+
 }
